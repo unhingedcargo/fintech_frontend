4ebaf6f2831cbb3e0d4b3b8c7dcc507f5c0232d2
unhingedcargo <unhingedcargo@gmail.com>
2025-08-20 19:41:57 +0530

    estimate I

---
+++ src/app/estimate/[jobno]/page.jsx
@@ -0,0 +1,56 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+
+
+export default function EstimateDetails({params}) {
+    const {jobno} = React.use(params);
+    const [jobData, setJobData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(`http://localhost:8000/api/estimate/${jobno}`); 
+        if (!res.ok) {
+          throw new Error(`Error ${res.status}`);
+        }
+        const json = await res.json();
+        setJobData(json);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    if (jobno) fetchData();
+  }, [jobno]);
+
+if (loading) return <p>Loading...</p>;
+if (error) return <p style={{ color: "red" }}>Error: {error}</p>;
+
+  return (
+    <>
+      <div className='pt-20'>
+        <h1>Job Number {jobno}</h1>
+
+        <p>{jobData.orders.map(item => (
+          <tr key={item.id}>
+            {item.id} <br/>
+            {item.item}<br/>
+            {item.desc}<br/>
+            {item.qty}<br/>
+            {item.rate}<br/>
+            {item.amount}<br/><br/><br/>
+          </tr>
+        ))}</p>
+        {/* <div className='mt-10 pt-10'>
+          <h1>Estimate Details for Job No: {jobno}</h1>
+          <pre>{JSON.stringify(jobData, null, 4)}</pre>
+        </div> */}
+      </div>
+    </>
+  )
+}
--- src/app/estimate/page.jsx
+++ src/app/estimate/page.jsx
@@ -1,10 +1,41 @@
+"use client"
 import Sidebar from '@/components/Sidebar'
 import Link from 'next/link'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+
+
 
 export default function EstimatePage() {
+  const[estimates, setEstimates] = useState([]);
+  const router = useRouter();
+
+  // useEffect(()=>{
+  //   await fetch("http://localhost:8000/api/jobcard")
+  //   .then((res) => res.json())
+  //   .then((data) => setEstimates(data));
+  // },[]);
+  
+
+  useEffect(() => {
+    // fetch data inside async function
+    const fetchEstimates = async () => {
+      try {
+        const res = await fetch("http://127.0.0.1:8000/api/jobcard");
+        if (!res.ok) throw new Error("Failed to fetch");
+        const data = await res.json();
+        setEstimates(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchEstimates();
+  }, []);
+
   return (
     <>
+    
     <div className='flex'>
       <div className="w-full md:w-64">
         <Sidebar />
@@ -14,25 +45,47 @@ export default function EstimatePage() {
           <h1 className='text-2xl mb-4'>Estimate page</h1>
           <Link href="/estimate/create-new" className='bg-blue-600 hover:bg-blue-300 text-white text-xl ms-auto me-0 rounded-md py-2 px-6'>New +</Link>
         </div>
-
-          <table className='min-w-full border border-collapse border-blue-400 text-lg mt-8'>
+          <table className='border border-collapse border-blue-400 text-base mt-8 w-[90%]'>
             <thead className='bg-blue-950'>
-              <tr>
-                <th className='border-2 border-blue-400 px-4 py-2 w-[10%]'>#</th>
-                <th className='border-2 border-blue-400 px-4 py-2 w-[15%]'>Date</th>
-                <th className='border-2 border-blue-400 px-4 py-2 w-[35%]'>Customer</th>
-                <th className='border-2 border-blue-400 px-4 py-2 w-[20%]'>Value</th>
-                <th className='border-2 border-blue-400 px-4 py-2 w-[20%]'>Status</th>
+              <tr className=''>
+                <th className='border-2 border-blue-400 font-normal px-4 py-3 w-[5%]'>#</th>
+                <th className='border-2 border-blue-400 font-normal px-4 py-3 w-[15%]'>Date</th>
+                <th className='border-2 border-blue-400 font-normal px-4 py-3 w-[20%]'>Customer</th>
+                <th className='border-2 border-blue-400 font-normal px-4 py-3 w-[20%]'>Contact</th>
+                <th className='border-2 border-blue-400 font-normal px-4 py-3 w-[10%]'>Value</th>
+                <th className='border-2 border-blue-400 font-normal px-4 py-3 w-[10%]'>Outstanding</th>
+                <th className='border-2 border-blue-400 font-normal px-4 py-3 w-[10%]'>Status</th>
+                <th className='border-2 border-blue-400 font-normal px-4 py-3 w-[5%]'>View</th>
+
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td className='border-2 border-blue-400 px-4 py-2 w-[10%]'>#</td>
-                <td className='border-2 border-blue-400 px-4 py-2 w-[15%]'>Date</td>
-                <td className='border-2 border-blue-400 px-4 py-2 w-[35%]'>Customer</td>
-                <td className='border-2 border-blue-400 px-4 py-2 w-[20%]'>Value</td>
-                <td className='border-2 border-blue-400 px-4 py-2 w-[20%]'>Status</td>
-              </tr>
+              {estimates.map((row,index) => (
+                <tr key={index+1} className='cursor-pointer' onClick={() => router.push(`/estimates/${row.jobno}`)}>
+                  <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[5%]'>{index+1}</td>
+                  <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[15%] text-center'>{row.job_date}</td>
+                  <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[20%]'>{row.cust_id}</td>
+                  <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[20%]'>{row.cust_id}</td>
+                  <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[10%] text-right'>{row.grandtotal}</td>
+                  <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[10%] text-right'>{row.grandtotal - row.advance}</td>
+                  <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[10%] text-center'>{row.jobno}</td>
+                  <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[5%] text-center'>
+                    <Link href="" className='bg-gray-800 px-4 py-2 rounded-md hover:text-amber-400 hover:bg-gradient-to-b'>View</Link>
+                  </td>
+                </tr>
+              ))}
+              {/* <tr>
+                <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[5%]'>#</td>
+                <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[15%] text-center'>Date</td>
+                <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[20%]'>Customer</td>
+                <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[20%]'>Contact</td>
+                <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[10%] text-right'>Value</td>
+                <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[10%] text-right'>Outstanding</td>
+                <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[10%] text-center'>Status</td>
+                <td className='border-2 border-blue-400 font-normal px-4 py-2 w-[5%] text-center'>
+                  <Link href="" className='bg-gray-800 px-4 py-2 rounded-md hover:text-amber-400 hover:bg-gradient-to-b'>View</Link>
+                  </td>
+              </tr> */}
             </tbody>
           </table>
       </div>
