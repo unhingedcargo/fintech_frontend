da106107687276c1bb3b4375d7761c48b0b2d0ca
unhingedcargo <unhingedcargo@gmail.com>
2025-09-13 18:03:10 +0530

    vendor rect - II

--- src/app/customer/page.jsx
+++ src/app/customer/page.jsx
@@ -1,6 +1,7 @@
+"use client"
+
 export const dynamic = "force-dynamic";
 
-"use client"
 import Sidebar from '@/components/Sidebar'
 import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation';
